639ed13a4e29462bffc100927bfe6e38a48b1d0f
Gustavo Basso <gubasso@eambar.net>
2024-04-10 14:05:15 -0300

    up

--- ebpf_program/programs/ebpf_program/src/lib.rs
+++ ebpf_program/programs/ebpf_program/src/lib.rs
@@ -6,11 +6,31 @@ declare_id!("CPy4XsWvCfZyjjfx3Uymf2y6BfN7vUHczb9dLYmPvSJc");
 pub mod ebpf_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Hello, Solana!");
+    pub fn initialize(ctx: Context<Initialize>, date: String) -> Result<()> {
+        let data_account = &mut ctx.accounts.data_account;
+        require!(date.len() == 10, ErrorCode::InvalidDate);
+        data_account.date = date.clone();
+        msg!("Date stored: {}", date);
         Ok(())
     }
 }
 
+#[account]
+pub struct DataAccount {
+    pub date: String, // YYYY-MM-DD
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 32)] // 8 bytes for the discriminator + 10 bytes for the date + 22 bytes for the string overhead
+    pub data_account: Account<'info, DataAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided date is not valid.")]
+    InvalidDate,
+}
--- sol_rbpf/src/main.rs
+++ sol_rbpf/src/main.rs
@@ -1,34 +1,37 @@
 use solana_rbpf::{
-    aligned_memory::AlignedMemory,
-    ebpf,
-    elf::Executable,
-    memory_region::{MemoryMapping, MemoryRegion},
-    program::{BuiltinProgram, FunctionRegistry, SBPFVersion},
-    verifier::RequisiteVerifier,
-    vm::{Config, EbpfVm, TestContextObject},
+    aligned_memory::AlignedMemory, ebpf, elf::Executable, memory_region::{MemoryMapping, MemoryRegion}, program::{BuiltinFunction, BuiltinProgram, FunctionRegistry}, syscalls::SyscallString, verifier::RequisiteVerifier, vm::{Config, EbpfVm, TestContextObject}
 };
 
-use std::fs::File;
+use std::{fs::File, sync::Arc};
 use std::io::Read;
 
 fn main() {
 
-    let mut file = File::open("ebpf_program.so").expect("Failed to open ELF file");
+    let mut file = File::open("../ebpf_program/target/sbf-solana-solana/release/ebpf_program.so").expect("Failed to open ELF file");
     let mut elf_bytes = Vec::new();
     file.read_to_end(&mut elf_bytes).expect("Failed to read ELF file");
 
-
-    // let mem = &mut [
-    //     0xaa, 0xbb, 0x11, 0x22, 0xcc, 0xdd
-    // ];
-    let mem = &mut [0u8; 1024];
-
-    let loader = std::sync::Arc::new(BuiltinProgram::new_mock());
-    // let function_registry = FunctionRegistry::default();
-    let mut executable = Executable::<TestContextObject>::from_elf(&mut elf_bytes, loader.clone()).unwrap();
+    let mut function_registry = FunctionRegistry::<BuiltinFunction<TestContextObject>>::default();
+    function_registry
+        .register_function_hashed(*b"log", SyscallString::vm)
+        .unwrap();
+    // let loader = std::sync::Arc::new(BuiltinProgram::new_mock());
+    let config = Config {
+        enable_instruction_meter: true,
+        instruction_meter_checkpoint_distance: 1_000_000,
+        ..Default::default()
+    };
+    let loader = Arc::new(BuiltinProgram::new_loader(
+        config,
+        function_registry,
+    ));
+    let executable = Executable::<TestContextObject>::from_elf(&elf_bytes, loader.clone()).unwrap();
     executable.verify::<RequisiteVerifier>().unwrap();
     let mut context_object = TestContextObject::new(1);
     let sbpf_version = executable.get_sbpf_version();
+
+    let mut date_str = "2024-04-10".to_string();
+    let date_bytes: &mut [u8] = unsafe { date_str.as_bytes_mut() };
 
     let mut stack = AlignedMemory::<{ebpf::HOST_ALIGN}>::zero_filled(executable.get_config().stack_size());
     let stack_len = stack.len();
@@ -41,7 +44,7 @@ fn main() {
         ebpf::MM_STACK_START,
         ),
         MemoryRegion::new_writable(heap.as_slice_mut(), ebpf::MM_HEAP_START),
-        MemoryRegion::new_writable(mem, ebpf::MM_INPUT_START),
+        MemoryRegion::new_writable(&mut *date_bytes, ebpf::MM_INPUT_START),
     ];
 
     let memory_mapping = MemoryMapping::new(regions, executable.get_config(), sbpf_version).unwrap();
@@ -51,4 +54,5 @@ fn main() {
     let (instruction_count, result) = vm.execute_program(&executable, true);
 
     println!("Program executed with {} instructions.", instruction_count);
+    println!("Result: {:#?}", result);
 }
